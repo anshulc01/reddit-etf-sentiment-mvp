de0098bb8b576f871390156488a379da39b9de88
anshulc01 <24ln25@queensu.ca>
2025-11-19 18:39:56 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -45,12 +45,20 @@ POST_LIMIT = 150
 def get_reddit_client():
     """Returns authenticated Reddit client using full script auth."""
 
- reddit = praw.Reddit(
-    client_id="o6Zs31tEtOrXylN63wLC2Q",         # Your 14-character client ID
-    client_secret="WDTefleTeB78pbJv-JXo49YE04tr4w", # Your 27-character client secret
-    user_agent="QueensCapstoneETF-sentiment/1.0 by u/CapstoneProject01"        # Your custom user agent string
-check_for_async=False,)
+    client_id = st.secrets["REDDIT_CLIENT_ID"]
+    client_secret = st.secrets["REDDIT_CLIENT_SECRET"]
+    user_agent = st.secrets["REDDIT_USER_AGENT"]
+    username = st.secrets["REDDIT_USERNAME"]
+    password = st.secrets["REDDIT_PASSWORD"]
 
+    reddit = praw.Reddit(
+        client_id=client_id,
+        client_secret=client_secret,
+        user_agent=user_agent,
+        username=username,
+        password=password,
+        check_for_async=False,
+    )
     return reddit
 
 
